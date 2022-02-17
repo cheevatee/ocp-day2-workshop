c4d6b7fed55dd44d6766ec9734a48c7e07ad7872
root <root@bastion.5m69d.internal>
2022-02-17 06:44:12 +0000

    Update config-ingress-access-logging.sh

--- network/router-access-logging/config-ingress-access-logging.sh
+++ network/router-access-logging/config-ingress-access-logging.sh
@@ -2,11 +2,28 @@ echo "
 
 ##### Add following to spec section of IngressController ##### 
 
+# Configure Ingress access logging to a sidecar.
+
 spec:
+  endpointPublishingStrategy:
+    type: NodePortService
   logging:
     access:
       destination:
         type: Container
+
+# Configure Ingress access logging to a Syslog endpoint.
+
+spec:
+  endpointPublishingStrategy:
+    type: NodePortService
+  logging:
+    access:
+      destination:
+        type: Syslog
+        syslog:
+          address: 1.2.3.4
+          port: 10514 
 "
 
 read -p "Press enter to continue!!!"
